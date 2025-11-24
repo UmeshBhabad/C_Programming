6e89df9dde5f7df3231836023af6a520019f519d
UmeshBhabad <umeshbhabad9@gmail.com>
2025-11-24 18:18:51 +0530

    Updated inputs problem31_4.c

--- Assignments/Assignment_31/program31_4.c
+++ Assignments/Assignment_31/program31_4.c
@@ -1,7 +1,7 @@
 // Accept number of rows and number of columns from user and display below pattern.
 
 /*
-    Input :     iRow = 4    iCol = 4
+    Input :     iRow = 6    iCol = 6
 
     Output :    *   *   *   *   *   *
                 *   *               *
