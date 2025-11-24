0458da875992c31b10ffce6a6915f1f941d39e12
UmeshBhabad <umeshbhabad9@gmail.com>
2025-11-24 17:51:04 +0530

    Added Assignment_30 Q1-Q5 (Problems on patterns)

---
+++ Assignments/Assignment_30/program30_5.c
@@ -0,0 +1,65 @@
+// Accept number of rows and number of columns from user and display below pattern.
+
+/*
+    Input :     iRow = 4    iCol = 4
+
+    Output :    1   2   3   4
+                1   *   *   4
+                1   *   *   4
+                1   2   3   4
+*/
+
+///////////////////////////////////////////////////////////////////////////////////////
+//
+// Header File
+//
+///////////////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+///////////////////////////////////////////////////////////////////////////////////////
+// 
+// Function     : Pattern
+// Description  : used to Display patterns
+// Input        : Integer, Integer
+// Output       : String
+// Author       : Umesh Shivaji Bhabad
+// Date         : 24/11/2025
+// 
+///////////////////////////////////////////////////////////////////////////////////////
+
+void Pattern(int iRow, int iCols)                       // Function Defination
+{
+    int i = 0, j = 0;                                   // Local Variables
+
+    for(i = 1; i <= iRow; i++)                          // Business Logic
+    {
+        for(j = 1; j <= iCols; j++)
+        {
+            if(i == 1 || i == iRow || j == 1 || j == iCols)
+            {
+                printf("%d\t", j);
+            }
+            else
+            {
+                printf("*\t");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()                                              // Main Method
+{
+    int iValue1 = 0, iValue2 = 0;                       // Local Variables
+
+    printf("Enter the Number of Rows : \n");            // Accept Rows
+    scanf("%d", &iValue1);
+
+    printf("Enter the Number of Columns : \n");         // Accept Columns
+    scanf("%d", &iValue2);
+
+    Pattern(iValue1, iValue2);                          // Function call
+
+    return 0;
+}
