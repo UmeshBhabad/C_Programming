9ff5d498585b5383bcef5bae30f2408076d3d7d4
UmeshBhabad <umeshbhabad9@gmail.com>
2025-11-24 15:19:41 +0530

    Added Assignment_27 Q1-Q5 (Problems on patterns)

---
+++ Assignments/Assignment_27/program27_2.c
@@ -0,0 +1,58 @@
+// Accept number of rows and number of columns from user and display below pattern.
+
+/*
+    Input :     iRow = 4    iCol = 3
+
+    Output :    1   2   3
+                1   2   3
+                1   2   3
+                1   2   3
+*/
+
+///////////////////////////////////////////////////////////////////////////////////////
+//
+// Header File
+//
+///////////////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+///////////////////////////////////////////////////////////////////////////////////////
+// 
+// Function     : Pattern
+// Description  : used to Display patterns
+// Input        : Integer, Integer
+// Output       : String
+// Author       : Umesh Shivaji Bhabad
+// Date         : 24/11/2025
+// 
+///////////////////////////////////////////////////////////////////////////////////////
+
+void Pattern(int iRow, int iCols)                       // Function Defination
+{
+    int i = 0, j = 0;                                   // Local Variables
+
+    for(i = 1; i <= iRow; i++)                          // Business Logic
+    {
+        for(j = 1; j <= iCols; j++)
+        {
+            printf("%d\t", j);
+        }
+        printf("\n");
+    }
+}
+
+int main()                                              // Main Method
+{
+    int iValue1 = 0, iValue2 = 0;                       // Local Variables
+
+    printf("Enter the Number of Rows : \n");            // Accept Rows
+    scanf("%d", &iValue1);
+
+    printf("Enter the Number of Columns : \n");         // Accept Columns
+    scanf("%d", &iValue2);
+
+    Pattern(iValue1, iValue2);                          // Function call
+
+    return 0;
+}
---
+++ Assignments/Assignment_27/program27_4.c
@@ -0,0 +1,64 @@
+// Accept number of rows and number of columns from user and display below pattern.
+
+/*
+    Input :     iRow = 3    iCol = 4
+
+    Output :    *   #   *   #
+                *   #   *   #
+                *   #   *   #
+*/
+
+///////////////////////////////////////////////////////////////////////////////////////
+//
+// Header File
+//
+///////////////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+///////////////////////////////////////////////////////////////////////////////////////
+// 
+// Function     : Pattern
+// Description  : used to Display patterns
+// Input        : Integer, Integer
+// Output       : String
+// Author       : Umesh Shivaji Bhabad
+// Date         : 24/11/2025
+// 
+///////////////////////////////////////////////////////////////////////////////////////
+
+void Pattern(int iRow, int iCols)                       // Function Defination
+{
+    int i = 0, j = 0;                                   // Local Variables
+
+    for(i = 1; i <= iRow; i++)                          // Business Logic
+    {
+        for(j = 1; j <= iCols; j++)
+        {
+            if(j % 2 == 1)
+            {
+                printf("*\t");
+            }
+            else
+            {
+                printf("#\t");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()                                              // Main Method
+{
+    int iValue1 = 0, iValue2 = 0;                       // Local Variables
+
+    printf("Enter the Number of Rows : \n");            // Accept Rows
+    scanf("%d", &iValue1);
+
+    printf("Enter the Number of Columns : \n");         // Accept Columns
+    scanf("%d", &iValue2);
+
+    Pattern(iValue1, iValue2);                          // Function call
+
+    return 0;
+}
