0bc48d7b6ba75607f10896d4d4418c66730ea1b8
UmeshBhabad <umeshbhabad9@gmail.com>
2026-01-04 03:01:42 +0530

    Added Assignment_48 Q1-Q2 (Problems on Linked List)

---
+++ Assignments/Assignment_48/program48_2.c
@@ -0,0 +1,153 @@
+// Replace Odd numbers with 0
+/*
+void ReplaceOdd(PPNODE Head);
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node *PNODE;
+typedef struct node **PPNODE;
+
+/*----------------------------------------------------------------------------------------------
+        Old Name                        New Name
+----------------------------------------------------------------------------------------------
+        struct node                     NODE
+        struct node *                   PNODE
+        struct node **                  PPNODE
+----------------------------------------------------------------------------------------------*/
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Function name    :   Display
+// Description      :   Used to display elements of Linked List
+// Parameters       :   First Pointer
+// Return Value     :   void
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+void Display(PNODE first)
+{
+    printf("\n");
+    while(first != NULL)
+    {
+        printf("| %d | -> ", first -> data);
+        first = first -> next;
+    }
+    printf("NULL\n");
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Function name    :   Count
+// Description      :   Used to count elements of Linked List
+// Parameters       :   First Pointer
+// Return Value     :   int
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+int Count(PNODE first)
+{
+    int iCount = 0;
+
+    while(first != NULL)
+    {
+        iCount++;
+        first = first -> next;
+    }
+
+    return iCount;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Function name    :   InsertLast
+// Description      :   Used to insert at last position of Linked List
+// Parameters       :   Address of First pointer & data of node
+// Return Value     :   void
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+void InsertLast(PPNODE first, int iNo)
+{
+    PNODE newn = NULL;
+    PNODE temp = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = iNo;
+    newn -> next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        temp = *first;
+
+        while(temp -> next != NULL)
+        {
+            temp = temp -> next;
+        }
+
+        temp -> next = newn;
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Function name    :   ReplaceOdd
+// Description      :   used to replace odd numbers with 0.
+// Parameters       :   First Pointer
+// Return Value     :   void
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+void ReplaceOdd(PPNODE first)
+{
+    PNODE temp = NULL;
+
+    temp = *first;
+
+    while(temp != NULL)
+    {       
+        if(temp -> data % 2 == 1)
+        {
+            temp -> data = 1;
+        }
+        temp = temp -> next;
+    }
+}
+
+int main()
+{
+    PNODE head = NULL;
+
+    int iRet = 0;
+
+    InsertLast(&head, 11);
+    InsertLast(&head, 20);
+    InsertLast(&head, 51);
+    InsertLast(&head, 101);
+    InsertLast(&head, 110);
+    InsertLast(&head, 121);
+
+    Display(head);
+
+    iRet = Count(head);
+    printf("Number of nodes are : %d\n", iRet);
+
+    ReplaceOdd(&head);
+
+    Display(head);
+
+    return 0;
+}
