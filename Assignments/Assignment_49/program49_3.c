6948a9e98a9e9ca988d826401ab5ecb5172495b3
UmeshBhabad <umeshbhabad9@gmail.com>
2026-01-04 03:02:09 +0530

    Added Assignment_49 Q1-Q2 (Problems on Linked List)

---
+++ Assignments/Assignment_49/program49_3.c
@@ -0,0 +1,149 @@
+// Display Elements Divisible by 3
+/*
+void DisplayDivByThree(PNODE Head);
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node *PNODE;
+typedef struct node **PPNODE;
+
+/*----------------------------------------------------------------------------------------------
+        Old Name                        New Name
+----------------------------------------------------------------------------------------------
+        struct node                     NODE
+        struct node *                   PNODE
+        struct node **                  PPNODE
+----------------------------------------------------------------------------------------------*/
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Function name    :   Display
+// Description      :   Used to display elements of Linked List
+// Parameters       :   First Pointer
+// Return Value     :   void
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+void Display(PNODE first)
+{
+    printf("\n");
+    while(first != NULL)
+    {
+        printf("| %d | -> ", first -> data);
+        first = first -> next;
+    }
+    printf("NULL\n");
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Function name    :   Count
+// Description      :   Used to count elements of Linked List
+// Parameters       :   First Pointer
+// Return Value     :   int
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+int Count(PNODE first)
+{
+    int iCount = 0;
+
+    while(first != NULL)
+    {
+        iCount++;
+        first = first -> next;
+    }
+
+    return iCount;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Function name    :   InsertLast
+// Description      :   Used to insert at last position of Linked List
+// Parameters       :   Address of First pointer & data of node
+// Return Value     :   void
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+void InsertLast(PPNODE first, int iNo)
+{
+    PNODE newn = NULL;
+    PNODE temp = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = iNo;
+    newn -> next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        temp = *first;
+
+        while(temp -> next != NULL)
+        {
+            temp = temp -> next;
+        }
+
+        temp -> next = newn;
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Function name    :   DisplayDivByThree
+// Description      :   Used to print elements divisible by 3 in the Linked List
+// Parameters       :   First Pointer
+// Return Value     :   void
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+void DisplayDivByThree(PNODE first)
+{
+    while(first != NULL)
+    {
+        if(first -> data % 3 == 0)
+        {
+            printf("%d\t", first -> data);
+        }
+
+        first = first -> next;
+    }
+}
+
+int main()
+{
+    PNODE head = NULL;
+
+    int iRet = 0;
+
+    InsertLast(&head, 11);
+    InsertLast(&head, 21);
+    InsertLast(&head, 51);
+    InsertLast(&head, 111);
+    InsertLast(&head, 113);
+    InsertLast(&head, 120);
+
+    Display(head);
+
+    iRet = Count(head);
+    printf("Number of nodes are : %d\n", iRet);
+
+    DisplayDivByThree(head);
+
+    return 0;
+}
